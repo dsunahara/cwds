4d8a8c2dcba8e77950d0c03a14921fe5b95901f9
Justin Kwong <justin.kwong@osi.ca.gov>
2017-01-24 23:32:52 +0000

    update project managment links

--- config/routes.rb
+++ config/routes.rb
@@ -17,10 +17,10 @@ Rails.application.routes.draw do
   get  '/vendors', to: 'static_pages#vendors'
   get  '/join_cwds', to: 'static_pages#join_cwds'
 
-  get  '/project_management_plan', to: 'static_pages/management_plans#project_management_plan'
-  get  '/requirements_management_plan', to: 'static_pages/management_plans#requirements_management_plan'
-  get  '/risk_and_issue_plan', to: 'static_pages/management_plans#risk_and_issue_plan'
-  get  '/schedule_management_plan', to: 'static_pages/management_plans#schedule_management_plan'
+  get  '/project_management_plan', to: 'static_pages#project_management_plan'
+  get  '/requirements_management_plan', to: 'static_pages#requirements_management_plan'
+  get  '/risk_and_issue_plan', to: 'static_pages#risk_and_issue_plan'
+  get  '/schedule_management_plan', to: 'static_pages#schedule_management_plan'
   
   get  '/cwds_github_website_standards', to: 'static_pages#cwds_github_website_standards'
   get  '/digital_service_design_standards', to: 'static_pages#digital_service_design_standards'
@@ -28,11 +28,8 @@ Rails.application.routes.draw do
   get  '/plan_template', to: 'static_pages#plan_template'
   
   
-  
-  
-  
-  
  resources :users
  resources :account_activations, only: [:edit]
   
 end
+
