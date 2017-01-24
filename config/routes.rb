9734367b0c93bbcd01be7b031b1ca1d01bc0008c
Justin Kwong <justin.kwong@osi.ca.gov>
2017-01-24 22:34:10 +0000

    Update how we operate

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,19 @@ Rails.application.routes.draw do
   get  '/vendors', to: 'static_pages#vendors'
   get  '/join_cwds', to: 'static_pages#join_cwds'
 
-
+  get  '/project_management_plan', to: 'static_pages/management_plans#project_management_plan'
+  get  '/requirements_management_plan', to: 'static_pages/management_plans#requirements_management_plan'
+  get  '/risk_and_issue_plan', to: 'static_pages/management_plans#risk_and_issue_plan'
+  get  '/schedule_management_plan', to: 'static_pages/management_plans#schedule_management_plan'
+  
+  get  '/cwds_github_website_standards', to: 'static_pages#cwds_github_website_standards'
+  get  '/digital_service_design_standards', to: 'static_pages#digital_service_design_standards'
+  get  '/project_management', to: 'static_pages#project_management'
+  get  '/plan_template', to: 'static_pages#plan_template'
+  
+  
+  
+  
   
   
   resources :users
