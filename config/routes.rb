8726b20eff482425d796bb059fcfdf8f052ec24a
dsunahara <darren.sunahara@osi.ca.gov>
2017-01-24 23:36:46 +0000

    Added vendors page and subpages

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,15 @@ Rails.application.routes.draw do
   get  '/stakeholders', to: 'static_pages#stakeholders'
   get  '/vendors', to: 'static_pages#vendors'
   get  '/join_cwds', to: 'static_pages#join_cwds'
-
-
-  
+  get  '/vendors/ADPQ_accepted_vendors', to: 'static_pages#ADPQ_accepted_vendors'
+  get  '/vendors/ADPQ_vendor_pool_RFI_announcement', to: 'static_pages#ADPQ_vendor_pool_RFI_announcement'
+  get  '/vendors/CALS_contract_execution', to: 'static_pages#CALS_contract_execution'
+  get  '/vendors/API_contract_execution', to: 'static_pages#API_contract_execution'
+  get  '/vendors/Intake_contract_execution', to: 'static_pages#Intake_contract_execution.html'
+  get  '/vendors/Intake_Implementation_bidders_conference', to: 'static_pages#Intake_Implementation_bidders_conference'
+  get  '/dashboard/CALS', to: 'static_pages#CALS'
+  get  '/dashboard/API', to: 'static_pages#API'
+  get  '/dashboard/Intake', to: 'static_pages#Intake'
   
   resources :users
   
