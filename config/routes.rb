5257aa615cc4a63ebd6534c71a772b207f073174
Andy <acatmpfer@gmail.com>
2017-02-21 01:19:35 +0000

    add-crud-to-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
   
+  
+  
+  resources :pages, only: [:show]
+  namespace :admin do 
+    resources :pages  # admin/pages 
+    
+  end
+  
+  #check and exclude all pages where slug is empty
+  Page.where.not(slug: nil).all.each do |page|
+    get "/#{page.slug}", controller: "pages", action:"show", id:page.id 
+  end
+  
+  
   get 'password_resets/new'
 
   get 'password_resets/edit'
