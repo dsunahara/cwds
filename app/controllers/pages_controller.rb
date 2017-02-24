987ffc9cee7095a969392214ef5181aae45ceb98
Andy <acatmpfer@gmail.com>
2017-02-24 00:48:14 +0000

    polished front end of page CRUD

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,10 @@
 class PagesController < ApplicationController
   
   def show
-    
-   # @page = Page.find(params[:id])
-   puts "testing"
-   puts params[:id]
    #@page = Page.find_by_id(params[:id])
    @page = Page.find_by_slug(params[:id])
-   redirect_to not_found_path unless @page
-
-    
+   
   end
+  
   
 end
