1b9661987c7883c5aeebd94ca706456ff7432fd5
Andy <acatmpfer@gmail.com>
2017-02-28 22:19:22 +0000

    added profile bar for logged in user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
   def show
    #@page = Page.find_by_id(params[:id])
    @page = Page.find_by_slug(params[:id])
-   
+    redirect_to root_path unless @page
   end
   
   
