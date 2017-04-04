b0ebe4cf39cb644fb3ede6b7e02a73978079ed1f
Andy <acatmpfer@gmail.com>
2017-04-04 22:25:33 +0000

    test merge update branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170313231959) do
+ActiveRecord::Schema.define(version: 20170330234851) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "user_id"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20170313231959) do
     t.string   "title"
     t.text     "body"
     t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
