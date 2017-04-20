f024a6b18382960fd68c3502a46f9fdfdde34dba
Andy <acatmpfer@gmail.com>
2017-04-20 23:08:52 +0000

    Added tag cloud

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,17 @@ module PostsHelper
   def tag_links(tags)
   tags.split(",").map{|tag| link_to tag.strip, tag_path(tag.strip) }.join(", ") 
   end
+  
   def category_links(categories)
   categories.split(",").map{|category| link_to category.strip, category_path(category.strip) }.join(", ") 
   end
+  
+  def tag_cloud(tags, classes)
+    max = tags.sort_by(&:count).last
+    tags.each do |tag|
+      index = tag.count.to_f / max.count * (classes.size-1)
+      yield(tag, classes[index.round])
+    end
+  end
+  
 end
