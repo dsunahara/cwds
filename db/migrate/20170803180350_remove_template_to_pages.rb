267029b9d256367e193eed7861a1fe31f405ca2b
dsunahara <darren.sunahara@osi.ca.gov>
2017-08-03 23:11:22 +0000

    Removed migration files

--- db/migrate/20170803180350_remove_template_to_pages.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTemplateToPages < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :pages, :template, :boolean
-  end
-end
--- db/migrate/20170803180436_remove_training_portal_to_pages.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTrainingPortalToPages < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :pages, :trainingPortal, :boolean
-  end
-end
