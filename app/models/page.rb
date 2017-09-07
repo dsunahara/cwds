6241566791cc6f6c0f0b961e37f3a871114669c1
Andy <acatmpfer@gmail.com>
2017-09-07 20:45:09 +0000

    added validations to page form

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,8 @@
 class Page < ApplicationRecord
+  
+  validates :title, presence: true, length: {minimum:5}
+  validates :body, presence: true
+  validates :slug, presence: true
     
   def to_param
    slug
